879e012a8e60a8bdd881efaaf0ebbf342b4c1d63
radhaorganisation <radhagithub@gmail.com>
2022-09-28 14:36:41 +0530

    reverted back revert"updated" Revert "test.py"
    This reverts commit 314937099ef1ac86eefd40a9c4faa43a0a5e87f7.

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,6 @@
 <<<<<<< HEAD
-<<<<<<< HEAD
 hi again i upadte this
 =======
 >>>>>>> parent of b83fc74 (updated deploy.sh)
 hello iam updating as per cr request
-=======
->>>>>>> parent of abdddc1 (updated deploy.sh)
 hello this file contains deployments
