314937099ef1ac86eefd40a9c4faa43a0a5e87f7
radhaorganisation <radhagithub@gmail.com>
2022-09-28 14:36:09 +0530

    test.py

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,9 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 hi again i upadte this
 =======
 >>>>>>> parent of b83fc74 (updated deploy.sh)
 hello iam updating as per cr request
+=======
+>>>>>>> parent of abdddc1 (updated deploy.sh)
 hello this file contains deployments
